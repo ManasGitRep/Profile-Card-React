0bb91747fe0d94cc40854cb9efb4138944fffa71
Manas Choudhary <mannchoudhary3431@gmail.com>
2024-06-27 17:46:59 +0530

    done adding components and styling to them

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -14,6 +13,37 @@ function App() {
   </div>;
 }
 
-function 
+function Avatar() {
+  return <img className="avatar" src="Sanji.jpg" alt="Manas Choudhary" />;
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Manas Choudhary</h1>
+      <p>
+        Passionate and skilled web developer with experience in designing,
+        developing, and maintaining websites and web applications.
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div>
+      <Skill skill="React" emoji="😛" color="#1E90FF" />
+      <Skill skill="Node" emoji="😒" color="#006400" />
+      <Skill skill="Express" emoji="😉" color="#F0FFFF" />
+      <Skill skill="Mongo" emoji="🙃" color="#7FFF00" />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill-list" style={{ backgroundColor: props.color }}>
+      <span>{Props.skill}</span>
+      <span>{Props.emoji}</span>
+    </div>
+  );
+}
 
 export default App;
